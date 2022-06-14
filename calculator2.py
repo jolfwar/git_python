3bb9cfa12be49c4922b7bb21583e653b053039bf
jolfwar <jolfwar@outlook.com>
2022-06-13 22:54:34 -0500

    Added comments.

--- calculator2.py
+++ calculator2.py
@@ -1,11 +1,14 @@
+# Displays and formats result of square and cube functions
 def main():
     x = float(input("What's x? "))
     print(f"x squared is {square(x):.2f}")
     print(f"x cubed is {cube(x):.2f}")
 
+# Squares the user input (x)
 def square(n):
     return pow(n, 2)
 
+# Cubes the user input (x)
 def cube(n):
     return pow(n, 3)
 
