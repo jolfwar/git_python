9c7e15a78c83755adecaf395afdaefd502bd10f5
jolfwar <jolfwar@outlook.com>
2022-06-13 21:36:46 -0500

    Simple division calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Prompt user for x and y variables
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Divide provided variables
+z = x / y
+
+# Format to the second decimal place
+print(f"{z:.2f}")
