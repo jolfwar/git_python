69acb848d25b5b4b19b21ffc8e54a5cc9ba3bae9
jolfwar <jolfwar@outlook.com>
2022-06-13 20:13:13 -0500

    Condensed code to fewer lines / formatting

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
 # Ask user for their name
-name = input("What's your name? ")
-planet = input("What planet are you located on? ")
-
-# Removes whitespace from strings
-name, planet = name.strip(), planet.strip()
-
-# Capitalize strings from user input
-name, planet = name.upper(), planet.upper()
+name = input("What's your name? ").strip().title()
+planet = input("What planet are you located on? ").strip().title()
 
 # Say hello to user
 print(f"Hello, {name}. ", end='')
