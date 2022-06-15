3fd4c4cdc6270b7ad0bcddea344208b66eae44ba
jolfwar <jolfwar@outlook.com>
2022-06-14 22:39:28 -0500

    Terminal Calculator

---
+++ terminalcalculator.py
@@ -0,0 +1,15 @@
+def main():
+    print("1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+    numOperator = int(input("Enter the corresponding number associated with an operator:"))
+    num1 = int(input("Enter the first number:"))
+    num2 = int(input("Enter the second number:"))
+
+def calcAdd():
+
+def calcSubtract():
+
+def calcMultiply():
+
+def calcDivide():
+
+main()
