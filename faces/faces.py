9c61eee80e3e74796200843a133a589750ecd22d
jolfwar <jolfwar@outlook.com>
2022-06-19 22:55:45 -0500

    Smiley converter

---
+++ faces/faces.py
@@ -0,0 +1,17 @@
+def main():
+    phrase = input("Enter a phrase: ")
+    emoji(phrase)
+
+def emoji(a):
+    a1 = a.replace(":)", "🙂")
+    a2 = a1.replace(":(", "🙁")
+    print(a2)
+
+
+main()
+
+
+
+
+
+#print("🙂")🙁
